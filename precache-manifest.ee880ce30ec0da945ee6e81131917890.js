44741ef2a935cf9be90fcd5d7d5a7702dc681fc6
MarkusZP <markslavenkoo@ukr.net>
2020-04-12 18:44:26 +0300

    Updates

---
+++ precache-manifest.ee880ce30ec0da945ee6e81131917890.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295e17145b175af5644f80bbb904b3a6",
+    "url": "/markslavenko.github.io/three-js-course/index.html"
+  },
+  {
+    "revision": "090b3b41bfc06f165c8d",
+    "url": "/markslavenko.github.io/three-js-course/static/css/2.402183b0.chunk.css"
+  },
+  {
+    "revision": "4069cd78cd509c9ad12b",
+    "url": "/markslavenko.github.io/three-js-course/static/css/main.29de6258.chunk.css"
+  },
+  {
+    "revision": "090b3b41bfc06f165c8d",
+    "url": "/markslavenko.github.io/three-js-course/static/js/2.10b7f4fe.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/markslavenko.github.io/three-js-course/static/js/2.10b7f4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4069cd78cd509c9ad12b",
+    "url": "/markslavenko.github.io/three-js-course/static/js/main.86c6e434.chunk.js"
+  },
+  {
+    "revision": "f25220eb89412524bf16",
+    "url": "/markslavenko.github.io/three-js-course/static/js/runtime-main.8f1f6f64.js"
+  },
+  {
+    "revision": "981a6ac07de4522c8347933d364207fd",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab1.981a6ac0.md"
+  },
+  {
+    "revision": "a9c6b86f147c9c99cd1387bcdbe4a263",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab10.a9c6b86f.md"
+  },
+  {
+    "revision": "23073f7749b30561c64642f2f1ed05de",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab2.23073f77.md"
+  },
+  {
+    "revision": "de684ce480ea48327dd967be6e745ca9",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab3.de684ce4.md"
+  },
+  {
+    "revision": "8bf0e1db311bc8da20e4476a83a36228",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab4.8bf0e1db.md"
+  },
+  {
+    "revision": "4de427a2cc249fd1bb97aabc88959230",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab5.4de427a2.md"
+  },
+  {
+    "revision": "3a36942483e82a0f1dcb5996d1b2dfc3",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab6.3a369424.md"
+  },
+  {
+    "revision": "7eb51da67e20246e46ec4be5708a0f86",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab7.7eb51da6.md"
+  },
+  {
+    "revision": "f6077a9c75f6e19aa75503fa4209cc51",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab8.f6077a9c.md"
+  },
+  {
+    "revision": "e92a4cac7cb4001d071a948ac04bf013",
+    "url": "/markslavenko.github.io/three-js-course/static/media/lab9.e92a4cac.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/markslavenko.github.io/three-js-course/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/markslavenko.github.io/three-js-course/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/markslavenko.github.io/three-js-course/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/markslavenko.github.io/three-js-course/static/media/slick.f97e3bbf.svg"
+  }
+]);
